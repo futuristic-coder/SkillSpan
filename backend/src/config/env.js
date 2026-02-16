270a8a175635d0c3b199d47ad9987ff2f8e854f8
futuristic-coder <raman17100100@gmail.com>
2026-02-16 17:26:06 +0530

    doing changes for signle deployment

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -2,5 +2,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const ENV={
-    PORT: process.env.PORT
+    PORT: process.env.PORT,
+    NODE_ENV: process.env.NODE_ENV
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,26 @@
 import express from 'express';
+import path from 'path';
 import { ENV } from './config/env.js';
 
 const app = express();
 
+const __dirname=path.resolve();
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
+
+app.get("/home", (req, res) => {
+    res.send("Raman World!");
+});
+
+if(ENV.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+    });
+}
 
 app.listen(ENV.PORT, () => {
     console.log(`Server is running on port ${ENV.PORT}`);
